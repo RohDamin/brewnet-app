d8568008c80e00b186b635b6ca6a4dac91805906
rohdamin <rohdamin3@gmail.com>
2025-02-04 18:08:49 +0900

    build: cors 설정 변경

--- src/main/java/com/varc/brewnetapp/config/WebCORSConfiguration.java
+++ src/main/java/com/varc/brewnetapp/config/WebCORSConfiguration.java
@@ -20,6 +20,7 @@ public class WebCORSConfiguration {
                 List.of(
                         "http://localhost:5173",
                         "http://brewnet.shop",
+                        "https://brewnet.shop",
                         "http://13.209.24.254",
                         "http://13.125.47.153:8080",
                         "http://localhost:8080"
